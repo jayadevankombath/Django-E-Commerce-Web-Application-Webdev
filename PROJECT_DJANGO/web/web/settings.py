ad26eb4fdf07c1c343a95d67ec3271dfe72389cf
jayadevan <jayadevankbh@gmail.com>
2022-02-07 10:07:22 +0530

    Added gitignore

--- PROJECT_DJANGO/web/web/settings.py
+++ PROJECT_DJANGO/web/web/settings.py
@@ -74,6 +74,12 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
